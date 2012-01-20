2407f394e9e467f992f595444a83fcde626ff576
Abhijith <abhijthg@gmail.com>
2012-01-20 22:31:55 +0530

    p14

---
+++ p14.clj
@@ -0,0 +1,11 @@
+;; Duplicate the elements of a list.
+;; Example:
+;; * (dupli '(a b c c d))
+;; (A A B B C C C C D D)
+
+(defn dupli
+  [[f & r :as coll] acc] 
+  (if (empty? coll)
+    (seq acc)
+    (dupli r (concat acc (repeat 2 f)))))
+        

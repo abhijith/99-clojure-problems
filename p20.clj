ce297a782502ca2075d7c6eb2fda39c8c2afb682
abhijith <abhijthg@gmail.com>
2012-01-28 18:08:32 +0530

    bug fix - handle case of zeroth index

--- p20.clj
+++ p20.clj
@@ -3,10 +3,11 @@
 ;; * (remove-at '(a b c d) 2)
 ;; (A C D)
 
+
 (defn remove-at
   ([[f & r :as coll] n cnt acc]
      (cond (empty? coll) (seq acc)
            (= n cnt)     (remove-at r n (inc cnt) acc)
            :else         (remove-at r n (inc cnt) (conj acc f))))
   ([coll n]
-     (remove-at coll n 1 [])))
+     (remove-at coll n 0 [])))
